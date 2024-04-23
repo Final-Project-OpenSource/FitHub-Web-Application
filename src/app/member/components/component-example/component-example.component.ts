746f6af103ef17331ff4b50a9122db0fb46e4337
Jorge Ponce <u202120471@upc.edu.pe>
2024-04-22 23:06:52 -0500

    fix(structure): corrections in project structure

---
+++ src/app/member/components/component-example/component-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-example.component.html',
+  styleUrl: './component-example.component.css'
+})
+export class ComponentExampleComponent {
+
+}

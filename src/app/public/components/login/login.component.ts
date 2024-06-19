c62fa71b8b85b276722871ef201dea2b0be19767
Jorge Ponce <91036361+Thealaskage@users.noreply.github.com>
2024-06-19 18:25:06 -0500

    feat: add validation to login, register and recovery

--- src/app/public/components/login/login.component.ts
+++ src/app/public/components/login/login.component.ts
@@ -1,23 +1,57 @@
 import {Component, ViewEncapsulation} from '@angular/core';
-import {MatFormField} from "@angular/material/form-field";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
 import {Router} from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   encapsulation: ViewEncapsulation.None,
-  imports: [MatFormField, MatButton, MatInput],
+  imports: [MatFormFieldModule, MatButton, MatInput, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router, private http: HttpClient) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
   onRegisterClick() {
     this.router.navigate(['register']);
   }
+
   onRecoveryPasswordClick() {
     this.router.navigate(['recovery-password']);
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.http.post('https://api.example.com/login', this.loginForm.value).subscribe((response: any) => {
+        // Suponiendo que la respuesta contiene el rol del usuario
+        const userRole = response.role;
+
+        // Redirigir basado en el rol
+        if (userRole === 'coach') {
+          this.router.navigate(['coach-dashboard']);
+        } else if (userRole === 'user') {
+          this.router.navigate(['user-dashboard']);
+        }
+
+        // Limpiar el formulario después del login
+        this.loginForm.reset();
+      }, error => {
+        console.error('Login failed', error);
+        // Manejo de errores (opcional)
+      });
+    }
   }
 }
--- src/app/public/components/recovery-password/recovery-password.component.ts
+++ src/app/public/components/recovery-password/recovery-password.component.ts
@@ -1,24 +1,54 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import {MatButton} from "@angular/material/button";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatError, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recovery-password',
   standalone: true,
-    imports: [
-        MatButton,
-        MatFormField,
-        MatInput,
-        MatLabel,
-        MatOption,
-        MatSelect
-    ],
+  imports: [
+    MatButton,
+    MatFormFieldModule,
+    MatInput,
+    MatLabel,
+    MatOption,
+    MatSelect,
+    ReactiveFormsModule,
+    CommonModule,
+    MatError
+  ],
   templateUrl: './recovery-password.component.html',
   styleUrl: './recovery-password.component.css'
 })
 export class RecoveryPasswordComponent {
+  recoveryForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.recoveryForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onRecover() {
+    if (this.recoveryForm.valid) {
+      this.http.put('https://api.example.com/recover-password', this.recoveryForm.value).subscribe({
+        next: response => {
+          console.log('Recovery successful', response);
+          this.recoveryForm.reset();
+          this.router.navigate(['login']);
+        },
+        error: error => {
+          console.error('Recovery failed', error);
+          this.errorMessage = 'Error al recuperar la contraseña. Por favor, inténtelo de nuevo.';
+        }
+      });
+    }
+  }
 }

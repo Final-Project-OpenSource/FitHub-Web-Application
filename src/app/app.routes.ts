840cdd2615692b19edab6dad7a74679ee583e49e
Jorge Ponce <91036361+Thealaskage@users.noreply.github.com>
2024-06-19 17:27:44 -0500

    feat: update router links for coach-dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,18 +10,29 @@ import { CoachProfileComponent } from "./coach/components/coach-profile/coach-pr
 import { MemberDashboardComponent} from "./member/components/member-dashboard/member-dashboard.component";
 import { MemberTrainingsComponent} from "./member/components/member-trainings/member-trainings.component";
 import {CoachDashboardComponent} from "./coach/components/coach-dashboard/coach-dashboard.component";
+import {CoachProgressComponent} from "./coach/components/coach-progress/coach-progress.component";
+import {CoachMemberListComponent} from "./coach/components/coach-member-list/coach-member-list.component";
+import {CoachPlansComponent} from "./coach/components/coach-plans/coach-plans.component";
+import {CoachNutritionComponent} from "./coach/components/coach-nutrition/coach-nutrition.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'recovery-password', component: RecoveryPasswordComponent},
+
+  {path: 'member-dashboard', component: MemberDashboardComponent},
   {path: 'coach', component: CoachInformationComponent},
   {path: 'contact-coach', component: CoachContactComponent},
-  {path: 'member-dashboard', component: MemberDashboardComponent},
   {path: 'member-trainings', component: MemberTrainingsComponent},
-  {path: 'coach-dashboard', component: CoachDashboardComponent},
   {path: 'member-profile', component: MemberProfileComponent},
+
+  {path: 'coach-dashboard', component: CoachDashboardComponent},
   {path: 'coach-profile', component: CoachProfileComponent},
+  {path: 'coach-progress', component: CoachProgressComponent},
+  {path: 'coach-member-list', component: CoachMemberListComponent},
+  {path: 'coach-plans', component: CoachPlansComponent},
+  {path: 'coach-profile', component: CoachProfileComponent},
+  {path: 'coach-nutrition-plan', component: CoachNutritionComponent},
   {path: '', pathMatch: 'full', component: LandingPageComponent},
   {path: '*', pathMatch: 'full', redirectTo: 'home'}
 ];
